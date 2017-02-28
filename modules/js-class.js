3cc3d4e3fe2daddba8b245a197ccb00af9dd0af6
Luke Harrison <luke@lukeharrison.net>
2017-02-28 09:04:41 +0000

    Completed JS-CLASS module

--- modules/js-class.js
+++ modules/js-class.js
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------------------------------------------------------------------------------
 
 /*
-JS-CLASS component allows you to quickly define class-based click events on elements.
+JS-CLASS component allows you to quickly define class-based click and/or swipe events on elements.
 
 You can define multiple behaviours by defining comma seperated lists. Each item corosponds to it's matching index in all the related data tags.
 
@@ -19,76 +19,122 @@ data-class-behaviour (Optional)
 	- "add": This adds the class if it's not present 
 	- "remove": This removes the class if it's present
 
-<div data-class="is-active, is-invalid, is-hidden" data-class-element="js-elem, js-elem2, js-elem3" data-class-behaviour="toggle, remove, add">
+data-class-swipe (Optional)
+- If defined, the specified swipe direction triggers class functionality. You have 4 choices for directions:-
+	- "up"
+	- "right"
+	- "down"
+	- "left"
+- You can also specify if or not the swipe event should replace the click event, or if both should coexist. To do this add a comma then either true or false after your direction.
+	- "true": Swipe event replaces click event
+	- "false": Swipe event and click event are both added
 
-In the above example, when our element is clicked the following happens:-
+<div data-class="is-active, is-invalid, is-hidden" data-class-element="js-elem, js-elem2, js-elem3" data-class-behaviour="toggle, remove, add" data-class-swipe="left, false">
+
+In the above example, when our element is either clicked or a left swipe is derected the following happens:-
 	1) is-active class is toggled on js-elem
 	2) is-invalid class is removed from js-elem2
 	3) is-hidden class is added to js-elem3 
 */ 
 
-(function(){  
+(function(){
 
-	// Grab all elements with required data-attributes and declare variables
-	var elems = document.querySelectorAll("[data-class][data-class-element]"),
+	// Import swipe helper
+	var swipeDetect = require("../helpers/swipeDetect.js"),
+	// Grab all elements with required data-attributes
+	elems = document.querySelectorAll("[data-class][data-class-element]"),
 	dataClass, 
 	dataClassElement,
 	dataClassBehaviour,
 	elem,
 	elemClass,
 	elemBehaviour,
+	elemSwipe,
+	elemSwipeBool,
+	direction,
+	currentElem,
+	elemRef,
 	a,
-	b;
+	b,
+	processClass = function(elem){
+		// Grab data-class data and convert to array
+		dataClass = elem.getAttribute("data-class");
+		dataClass = dataClass.split(", ");
+
+		// Grab data-class-element data
+		dataClassElement = elem.getAttribute("data-class-element");
+		dataClassElement = dataClassElement.split(", ");
+
+		// Grab data-class-behaviour if present
+		if(elem.getAttribute("data-class-behaviour")) {
+			dataClassBehaviour = elem.getAttribute("data-class-behaviour");
+			dataClassBehaviour = dataClassBehaviour.split(", ");
+		}
+
+		// Loop through all our dataClassElement items
+		for(b = 0; b < dataClassElement.length; b++) {
+			// Grab elem reference
+			elemRef = document.querySelector("." + dataClassElement[b]);
+			// Grab class we will add
+			elemClass = dataClass[b];
+			// Grab behaviour if any exists
+			if(dataClassBehaviour) {
+				elemBehaviour = dataClassBehaviour[b];
+			}
+			// Do
+			if(elemBehaviour === "add") {
+				if(!elemRef.classList.contains(elemClass)) {
+					elemRef.classList.add(elemClass);
+				}
+			}
+			else if(elemBehaviour === "remove") {
+				if(elemRef.classList.contains(elemClass)) {
+					elemRef.classList.remove(elemClass);
+				}
+			}
+			else {
+				elemRef.classList.toggle(elemClass);
+			}
+		}
+	};
 
 	// Only go ahead if we've found any matches
 	if (elems.length) {
 		// Loop through our matches and add click events
 		for(a = 0; a < elems.length; a++){
-			elems[a].addEventListener("click", function(e){
+			// Detect data-swipe attribute
+			if(elems[a].getAttribute("data-class-swipe")){
+				elemSwipe = elems[a].getAttribute("data-class-swipe");
+				elemSwipe = elemSwipe.split(", ");
+				direction = elemSwipe[0];
+				elemSwipeBool = elemSwipe[1];
+				currentElem = elems[a];
 
-				// Prevent default action of element
-				e.preventDefault();	
-
-				// Grab data-class data and convert to array
-				dataClass = this.getAttribute("data-class");
-				dataClass = dataClass.split(", ");
-
-				// Grab data-class-element data
-				dataClassElement = this.getAttribute("data-class-element");
-				dataClassElement = dataClassElement.split(", ");
-
-				// Grab data-class-behaviour if present
-				if(this.getAttribute("data-class-behaviour")) {
-					dataClassBehaviour = this.getAttribute("data-class-behaviour");
-					dataClassBehaviour = dataClassBehaviour.split(", ");
+				if(elemSwipeBool === "false") {
+					// Assign click event
+					elems[a].addEventListener("click", function(e){
+						// Prevent default action of element
+						e.preventDefault();	
+						// Run class function
+						processClass(this);
+					});
 				}
-
-				// Loop through all our dataClassElement items
-				for(b = 0; b < dataClassElement.length; b++) {
-					// Grab elem reference
-					elem = document.querySelector("." + dataClassElement[b]);
-					// Grab class we will add
-					elemClass = dataClass[b];
-					// Grab behaviour if any exists
-					if(dataClassBehaviour) {
-						elemBehaviour = dataClassBehaviour[b];
+				swipeDetect(elems[a], function(swipedir){
+					if(swipedir === direction) {
+						// Run class function
+						processClass(currentElem);
 					}
-					// Do
-					if(elemBehaviour === "add") {
-						if(!elem.classList.contains(elemClass)) {
-							elem.classList.add(elemClass);
-						}
-					}
-					else if(elemBehaviour === "remove") {
-						if(elem.classList.contains(elemClass)) {
-							elem.classList.remove(elemClass);
-						}
-					}
-					else {
-						elem.classList.toggle(elemClass);
-					}
-				}
-			});
+				})
+			}
+			else {
+				// Assign click event
+				elems[a].addEventListener("click", function(e){
+					// Prevent default action of element
+					e.preventDefault();	
+					// Run class function
+					processClass(this);
+				});
+			}
 		}
 	}
 })();
